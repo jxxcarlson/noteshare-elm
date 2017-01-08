1438aff7362178c4bc094c65a65989a1fd0643c0
James Carlson <jxxcarlson@gmail.com>
2017-01-08 12:12:39 -0500

    Initial commit: skeleton app which retrieves a document by ID and displays it -- No MathJax processing

---
+++ noteshare.elm
@@ -0,0 +1,109 @@
+import Html exposing (..)
+import Html.Attributes exposing (..)
+import Html.Events exposing (..)
+import Http
+import Json.Decode as Decode
+import Json.Encode as Encode exposing (string)
+import Json.Decode.Pipeline as Pipeline exposing (decode, required, optional, hardcoded)
+
+import Task
+
+
+
+main =
+  Html.program
+    { init = init
+    , view = view
+    , update = update
+    , subscriptions = subscriptions
+    }
+
+init : (Document, Cmd Msg)
+init =
+  ( Document "0" "Introductory Magick" "Don't believe a single word of it!" "Don't believe a <b>single</b> word of it!"
+  , Cmd.none
+  )
+-- MODEL
+
+type alias Document =
+  { id : String
+  , title : String
+  , text : String
+  , renderedText: String
+  }
+{-}
+documentDecode : Decode.Decoder  Document
+documentDecode =
+  decode Document
+    |> Pipeline.required "id" string
+    |> Pipeline.required "title" string-- `null` decodes to `Nothing`
+    |> Pipeline.required "rendered_text" string--
+-}
+
+--UPDATE
+
+type Msg
+  = None | SetId String| GetDocument | LoadDocument (Result Http.Error String)
+
+update : Msg -> Document -> (Document, Cmd Msg)
+update msg model =
+  case msg of
+    None ->
+      ( model, Cmd.none )
+
+    SetId id ->
+        ({ model | id = id}, Cmd.none  )
+
+    GetDocument ->
+      (model, getDocument model.id)
+
+    LoadDocument (Ok payload) ->
+      ({ model | renderedText = payload }, Cmd.none)
+
+    LoadDocument (Err _) ->
+      ({ model | renderedText = "Error for document " ++ model.id }, Cmd.none)
+
+
+
+-- VIEW
+
+-- mathJaxPrefix = "<script type=\"text/javascript\" async src=\"https://cdn.mathjax.org/mathjax/latest/MathJax.js?config=TeX-MML-AM_CHTML\"></script>"
+-- mathJaxPrefix= "<scriipt type='text/javascript'" ++ " async src='https://cdn.mathjax.org/mathjax/latest/MathJax.js?config=TeX-MML-AM_CHTML'>" ++ " </scriipt>"
+--  async src='https://cdn.mathjax.org/mathjax/latest/MathJax.js?config=TeX-MML-AM_CHTML'></script>"
+-- mathJaxPrefix = "foo"
+
+view : Document -> Html Msg
+view model =
+  div [Html.Attributes.style [("margin", "40px")]]
+    [
+     button [ onClick GetDocument ] [text "Get"]
+     , input [ type_ "text", placeholder "ID", onInput SetId ] []
+    --  p [ ] [ text model.error]
+    , br [] []
+    ,h2 [] [text model.title]
+    , br [] []
+    , div [ (Html.Attributes.property "innerHTML" (Encode.string model.renderedText)) ] []
+    ]
+
+-- HTTP
+
+-- id = 225
+
+getDocument : String -> Cmd Msg
+getDocument id =
+  let
+    url =
+      "http://xdoc-api.herokuapp.com/v1/documents/" ++ id ++ "?toc"
+  in
+    Http.send LoadDocument (Http.get url decodeDocument)
+
+decodeDocument : Decode.Decoder String
+decodeDocument =
+  Decode.at ["document", "rendered_text"] Decode.string
+
+
+-- SUBSCRIPTIONS
+
+subscriptions : Document -> Sub Msg
+subscriptions model =
+  Sub.none
