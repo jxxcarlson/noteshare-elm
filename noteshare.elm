8ace726470e14b61e9689197d66bc4415c85da14
James Carlson <jxxcarlson@gmail.com>
2017-01-14 10:08:19 -0500

    Decode nested json document package

--- noteshare.elm
+++ noteshare.elm
@@ -5,11 +5,12 @@ import Html.Attributes exposing (id, class, classList, src, name, type_, title, 
 import Html.Events exposing (..)
 import Http
 
-import Json.Encode as Encode
+import Json.Encode
+-- import Json.Encode as Encode
 import Json.Decode exposing (string, int, list, Decoder)
 import Json.Decode.Pipeline exposing (decode, required, optional)
 
-import Task
+
 
 
 main : Program Never Document Msg
@@ -23,20 +24,27 @@ main =
 
 init : (Document, Cmd Msg)
 init =
-  ( Document "0" "Introductory Magick" "Don't believe a single word of it!" "Don't believe a <b>single</b> word of it!"
+  ( Document 76 "Introductory Magick" "asciidoc-latex" False 39 "jc" False "2017" "2017" "Don't believe a single word of it!" "Don't believe a <b>single</b> word of it!"
   , Cmd.none
   )
 
   
 -- MODEL
 
-type alias Document =
-  { id : String
-  , title : String
-  , text : String
-  , renderedText: String
-  }
 
+type alias Document =
+    { id : Int
+    , title : String
+    , kind : String
+    , has_subdocuments : Bool
+    , owner_id : Int
+    , author : String
+    , public : Bool
+    , created_at : String
+    , updated_at : String
+    , text : String
+    , rendered_text : String
+    }
 
 --UPDATE
 
@@ -44,7 +52,7 @@ type Msg
   = None
   | SetId String
   | GetDocument
-  | LoadDocument (Result Http.Error String)
+  | LoadDocument (Result Http.Error DocumentPayload)
 
 --| LoadDocument (Result Http.Error String)
 --  = None | SetId String| GetDocument | LoadDocument (Result Http.Error Document)
@@ -56,16 +64,24 @@ update msg model =
       ( model, Cmd.none )
 
     SetId id ->
-        ({ model | id = id}, Cmd.none  )
+        (model, Cmd.none )
+        -- ( Document { model | id = id }, Cmd.none )
+        -- ( model, Cmd.none )
+        -- ( Document {model | id <- id }, Cmd.none )
+        -- ( model | model.id = id, Cmd.none  )
 
     GetDocument ->
-      (model, getDocument model.id)
+       (model, getDocument model.id)
 
     LoadDocument (Ok payload) ->
-      ({ model | renderedText = payload }, render payload)
+       -- ( model, Cmd.none )
+       let doc = payload.document
+
+       in ( doc, render doc.rendered_text)
 
     LoadDocument (Err error) ->
-      handleError model error
+       ( model, Cmd.none )
+       -- handleError model error
 
 
 
@@ -89,42 +105,78 @@ view model =
 
 renderedText : { a | renderedText : String } -> Attribute msg
 renderedText model =
-    (Html.Attributes.property "innerHTML" (Encode.string model.renderedText))
+    (Html.Attributes.property "innerHTML" (Json.Encode.string model.renderedText))
+-- "http://xdoc-api.herokuapp.com/v1"
+
+
+-- DECODERS
+
+
+type alias DocumentPayload =
+    { status : String
+    , document : Document
+    }
+
+
+decodeDocumentPayload : Json.Decode.Decoder DocumentPayload
+decodeDocumentPayload =
+    Json.Decode.Pipeline.decode DocumentPayload
+        |> Json.Decode.Pipeline.required "status" (Json.Decode.string)
+        |> Json.Decode.Pipeline.required "document" (decodeDocument)
+
+decodeDocument : Json.Decode.Decoder Document
+decodeDocument =
+    Json.Decode.Pipeline.decode Document
+        |> Json.Decode.Pipeline.required "id" (Json.Decode.int)
+        |> Json.Decode.Pipeline.required "title" (Json.Decode.string)
+        |> Json.Decode.Pipeline.required "kind" (Json.Decode.string)
+        |> Json.Decode.Pipeline.required "has_subdocuments" (Json.Decode.bool)
+        |> Json.Decode.Pipeline.required "owner_id" (Json.Decode.int)
+        |> Json.Decode.Pipeline.required "author" (Json.Decode.string)
+        |> Json.Decode.Pipeline.required "public" (Json.Decode.bool)
+        |> Json.Decode.Pipeline.required "created_at" (Json.Decode.string)
+        |> Json.Decode.Pipeline.required "updated_at" (Json.Decode.string)
+        |> Json.Decode.Pipeline.required "text" (Json.Decode.string)
+        |> Json.Decode.Pipeline.required "rendered_text" (Json.Decode.string)
+
+encodeDocumentPayload : DocumentPayload -> Json.Encode.Value
+encodeDocumentPayload record =
+    Json.Encode.object
+        [ ("status",  Json.Encode.string <| record.status)
+        , ("document",  encodeDocument <| record.document)
+        ]
+
+encodeDocument : Document -> Json.Encode.Value
+encodeDocument record =
+    Json.Encode.object
+        [ ("id",  Json.Encode.int <| record.id)
+        , ("title",  Json.Encode.string <| record.title)
+        , ("kind",  Json.Encode.string <| record.kind)
+        , ("has_subdocuments",  Json.Encode.bool <| record.has_subdocuments)
+        , ("owner_id",  Json.Encode.int <| record.owner_id)
+        , ("author",  Json.Encode.string <| record.author)
+        , ("public",  Json.Encode.bool <| record.public)
+        , ("created_at",  Json.Encode.string <| record.created_at)
+        , ("updated_at",  Json.Encode.string <| record.updated_at)
+        , ("text",  Json.Encode.string <| record.text)
+        , ("rendered_text",  Json.Encode.string <| record.rendered_text)
+        ]
+
 
 -- HTTP
-
--- "http://localhost:2300/v1"
--- "http://xdoc-api.herokuapp.com/v1"
 
 apiServerUrl : String
 apiServerUrl = "http://localhost:2300/v1"
 -- apiServerUrl = "http://xdoc-api.herokuapp.com/v1"
 
-documentDecoder2 : Decoder Document
-documentDecoder2 =
-    decode buildDocument
-        |> required "id" string
-        |> required "title" string
-        |> required "text" string
-        |> required "rendered_text" string
-
-buildDocument : String -> String -> String -> String -> Document
-buildDocument id title text rendered_text =
-    Document id title text rendered_text
-    -- { id = id, title = title, text = text, rendered_text = rendered_text }
-
-
-documentDecoder : Decoder String
-documentDecoder =
-  Json.Decode.at ["document", "rendered_text"] string
-
-getDocument : String -> Cmd Msg
+getDocument : Int -> Cmd Msg
 getDocument id =
   let
     url =
-      apiServerUrl ++ "/documents/" ++ id ++ "?toc"
+      apiServerUrl ++ "/documents/" ++ toString(id) ++ "?toc"
   in
-    Http.send LoadDocument (Http.get url documentDecoder)
+    Http.send LoadDocument (Http.get url decodeDocumentPayload)
+    -- Http.send LoadDocument (Http.get url documentDecoder)
 
 
 -- SUBSCRIPTIONS
---
+++ scratch.elm
@@ -0,0 +1,62 @@
+type alias DocumentPayload =
+    { status : String
+    , document : Document
+    }
+
+type alias Document =
+    { id : Int
+    , title : String
+    , kind : String
+    , has_subdocuments : Bool
+    , owner_id : Int
+    , author : String
+    , public : Bool
+    , created_at : String
+    , updated_at : String
+    , text : String
+    , rendered_text : String
+    }
+
+decodeDocumentPayload : Json.Decode.Decoder DocumentPayload
+decodeDocumentPayload =
+    Json.Decode.Pipeline.decode DocumentPayload
+        |> Json.Decode.Pipeline.required "status" (Json.Decode.string)
+        |> Json.Decode.Pipeline.required "document" (decodeDocument)
+
+decodeDocument : Json.Decode.Decoder Document
+decodeDocument =
+    Json.Decode.Pipeline.decode Document
+        |> Json.Decode.Pipeline.required "id" (Json.Decode.int)
+        |> Json.Decode.Pipeline.required "title" (Json.Decode.string)
+        |> Json.Decode.Pipeline.required "kind" (Json.Decode.string)
+        |> Json.Decode.Pipeline.required "has_subdocuments" (Json.Decode.bool)
+        |> Json.Decode.Pipeline.required "owner_id" (Json.Decode.int)
+        |> Json.Decode.Pipeline.required "author" (Json.Decode.string)
+        |> Json.Decode.Pipeline.required "public" (Json.Decode.bool)
+        |> Json.Decode.Pipeline.required "created_at" (Json.Decode.string)
+        |> Json.Decode.Pipeline.required "updated_at" (Json.Decode.string)
+        |> Json.Decode.Pipeline.required "text" (Json.Decode.string)
+        |> Json.Decode.Pipeline.required "rendered_text" (Json.Decode.string)
+
+encodeDocumentPayload : DocumentPayload -> Json.Encode.Value
+encodeDocumentPayload record =
+    Json.Encode.object
+        [ ("status",  Json.Encode.string <| record.status)
+        , ("document",  encodeDocument <| record.document)
+        ]
+
+encodeDocument : Document -> Json.Encode.Value
+encodeDocument record =
+    Json.Encode.object
+        [ ("id",  Json.Encode.int <| record.id)
+        , ("title",  Json.Encode.string <| record.title)
+        , ("kind",  Json.Encode.string <| record.kind)
+        , ("has_subdocuments",  Json.Encode.bool <| record.has_subdocuments)
+        , ("owner_id",  Json.Encode.int <| record.owner_id)
+        , ("author",  Json.Encode.string <| record.author)
+        , ("public",  Json.Encode.bool <| record.public)
+        , ("created_at",  Json.Encode.string <| record.created_at)
+        , ("updated_at",  Json.Encode.string <| record.updated_at)
+        , ("text",  Json.Encode.string <| record.text)
+        , ("rendered_text",  Json.Encode.string <| record.rendered_text)
+        ]
